a596bc47c1474d844afdf467217845c124522d2e
Taraansh <taraanshnandwani123@gmail.com>
2023-09-01 16:41:29 +0530

    Added PORT to run server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+//rest object
+const app = express()
+
+//rest api
+app.get('/', (req, res)=>{
+    res.send({message: "Welcome to ShopMonk app"})
+})
+
+//PORT
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`)
+  })
