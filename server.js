900751098022c952ac6e0065f35192dc3145c7f1
Taraansh <taraanshnandwani123@gmail.com>
2023-09-02 13:33:54 +0530

    Connected to db and added morgan to track requests

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //Configure env
 dotenv.config();
 
+//Database
+connectDB();
+
 //rest object
-const app = express()
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan("dev"));
 
 //rest api
-app.get('/', (req, res)=>{
-    res.send({message: "Welcome to ShopMonk app"})
-})
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to ShopMonk app" });
+});
 
 //PORT
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`)
-  })
+  console.log(`Server Running on ${PORT}`);
+});
