8260ab13c35a4c3f685546949c695a24ca9825db
Taraansh <taraanshnandwani123@gmail.com>
2023-09-03 09:07:35 +0530

    Added loginController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "./../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
+//Register Controller
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
@@ -49,13 +51,64 @@ export const registerController = async (req, res) => {
       message: "User Registered Successfully",
       user,
     });
-
-
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
       message: "Error in registeration",
+      error,
+    });
+  }
+};
+
+//Login Controller
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Email is not registered" });
+    }
+
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Invalid Password" });
+    }
+
+    //token
+    const token = JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error Loging in.",
       error,
     });
   }
