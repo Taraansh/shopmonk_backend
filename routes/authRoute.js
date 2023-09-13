fa81790285c0f423fcadbc8bd78768978a2e576e
Taraansh <taraanshnandwani123@gmail.com>
2023-09-13 20:26:21 +0530

    Added test route and protected route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   registerController,
   loginController,
+  testController,
 } from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -13,5 +15,15 @@ router.post("/register", registerController);
 
 //LOGIN || METHOD: POST
 router.post("/login", loginController);
+
+//TEST Routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//Protected route
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({
+    ok: true,
+  });
+});
 
 export default router;
