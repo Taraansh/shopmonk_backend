331a27612135f3afdbfbcf4344d056f15c37ca76
Taraansh <taraanshnandwani123@gmail.com>
2023-09-13 20:25:36 +0530

    Added testController

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,19 +8,19 @@ export const registerController = async (req, res) => {
     const { name, email, password, phone, address } = req.body;
     //validation
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
-      return res.send({ error: "Email is Required" });
+      return res.send({ message: "Email is Required" });
     }
     if (!password) {
-      return res.send({ error: "Password is Required" });
+      return res.send({ message: "Password is Required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone No. is Required" });
+      return res.send({ message: "Phone No. is Required" });
     }
     if (!address) {
-      return res.send({ error: "Address is Required" });
+      return res.send({ message: "Address is Required" });
     }
 
     //Check user
@@ -29,7 +29,7 @@ export const registerController = async (req, res) => {
     //existing user
     if (existingUser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "User is Already Registered. Please Login",
       });
     }
@@ -103,7 +103,6 @@ export const loginController = async (req, res) => {
       },
       token,
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -112,4 +111,9 @@ export const loginController = async (req, res) => {
       error,
     });
   }
+};
+
+//Test controller
+export const testController = (req, res) => {
+  res.send("Protected Router");
 };
