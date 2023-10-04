9fe120f6b016549e00d98ecaff873d42e2ff770d
Taraansh <taraanshnandwani123@gmail.com>
2023-10-04 20:17:05 +0530

    Routes

---
+++ routes/productRoute.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+import {
+  createProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  deleteProductController,
+  updateProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//router
+//create product
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//get all products
+router.get("/get-product", getProductController);
+
+//get single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete(
+  "/delete-product/:pid",
+  requireSignIn,
+  isAdmin,
+  deleteProductController
+);
+
+//update product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+export default router;
