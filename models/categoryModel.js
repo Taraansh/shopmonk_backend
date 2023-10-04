91e8382ff43b82f6dd17faa3f81541d57637be3f
Taraansh <taraanshnandwani123@gmail.com>
2023-10-04 20:16:52 +0530

    Model

---
+++ models/categoryModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  slug: { type: String, lowercase: true },
+});
+
+export default mongoose.model("Category", categorySchema);
