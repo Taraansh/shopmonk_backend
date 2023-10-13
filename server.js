e2b0ed96f48492c0a1a31706b274328c9d100737
Taraansh <taraanshnandwani123@gmail.com>
2023-10-13 15:32:21 +0530

    fix

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //Configure env
 dotenv.config();
 
 //Database
 connectDB();
+
+//es module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
