83d2b667ff441a641e0ace2b778b2f45a37dbabf
Taraansh <taraanshnandwani123@gmail.com>
2023-10-13 18:10:02 +0530

    Fix

--- server.js
+++ server.js
@@ -7,17 +7,12 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
 
 //Configure env
 dotenv.config();
 
 //Database
 connectDB();
-
-//es module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -26,17 +21,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
-//rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 //PORT
 const PORT = process.env.PORT;
